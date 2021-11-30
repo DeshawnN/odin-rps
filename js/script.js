99f8b299b3aebf49fa6cdb1221fa42c28a0e2bf3
Deshawn Nelson <Deshawn.a.nelson@gmail.com>
2021-11-30 03:56:07 -0500

    feat: add logic for playing a round of rps

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ const playerSelection = makeSelection();
 
 console.log(playRound(playerSelection, computerSelection));
 
+function playRound(playerSelection, computerSelection) {
+    const winMessage = `You win! ${playerSelection} beats ${computerSelection}`;
+    const loseMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
 
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") return loseMessage;
+        else if (computerSelection === "Scissors") return winMessage; 
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Scissors") return loseMessage;
+        else if (computerSelection === "Rock") return winMessage;
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") return loseMessage;
+        else if (computerSelection === "Paper") return winMessage;
+    }
+    
+}
 
 function computerPlay() {
     const choices = ["Rock", "Paper", "Scissors"];
